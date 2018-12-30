06e92dd5427a1594fe00c744c53ddd5f76f89e30
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-12-30 12:19:19 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Dark Rose Club
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <small>coming soon suck my dick</small>
         </header>
       </div>
     );
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Nav} from 'react-bootstrap'
+
+
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
