1c9d5f0f59aa3adab5c6d80e803c3dabcb600f60
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-12-30 13:44:33 -0700

    react strap styling

--- src/Nav.js
+++ src/Nav.js
@@ -1,36 +1,33 @@
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
 import React, { Component } from 'react'
+import './nav.css';
+
 
 export default class componentName extends Component {
   render() {
     return (
       <div>
-
-<Navbar inverse collapseOnSelect>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#brand">Dark Rose Club</a>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} href="#">
-        About
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Scheudle
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Members Only
-      </NavItem>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>;
-
-
-
-
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">Dark Rose Club</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                About
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Scheudle
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Members Only
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     )
   }
